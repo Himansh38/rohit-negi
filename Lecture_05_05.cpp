725c344713710b5d0a0f4a552abe815feefd6553
Himanshu Gupta <himanshgupta098@gmail.com>
2023-10-04 23:16:04 +0530

    modified

--- Lecture_05_05.cpp
+++ Lecture_05_05.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 int main()
@@ -7,24 +6,10 @@ int main()
     int num =n; 
     cout<<"Enter the number and square of the number ";
     cin >> n>> sq;
-    for (int i =1;i <= sq; i++){
+    for (int i =1;i < sq; i++){
         num = num*n;
     }
     cout<< "Squared value is :"<<num;
+    
     return 0 ;
-=======
-#include<iostream>
-using namespace std;
-int main()
-{
-    int n, sq;
-    int num =n; 
-    cout<<"Enter the number and square of the number ";
-    cin >> n>> sq;
-    for (int i =1;i <= sq; i++){
-        num = num*n;
-    }
-    cout<< "Squared value is :"<<num;
-    return 0 ;
->>>>>>> 8ba45e8ce026d137ccedf89dd2acc38db120d266
 }
