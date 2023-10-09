b4f0a91019be60834c6e98945cff0f4d4afa44d2
Himanshu Gupta <himanshgupta098@gmail.com>
2023-10-09 19:11:47 +0530

    Lecture 14 learning of day 18

--- Lecture_18_01.cpp
+++ Lecture_18_01.cpp
@@ -1,7 +1,41 @@
+// Lecture_18_01.cpp
 #include<iostream>
-using namespace std;
-int main()
-{
 
+using namespace std;
+
+int prime(int n)
+{
+   if (n<2) 
+    return 0;
+
+    for ( int i =2; i<n; i++)
+    {
+        if ( n%i== 0)
+        return 0;
+    }
+    return 1;
+}
+int fact(int n )
+{
+    int ans =1;
+    for ( int i=1; i<n; i++)
+    {
+        ans = ans *i;
+    }
+    return ans;
+}
+int main() 
+{
+    int a, b ;
+    cout << " Enter two numbers"<< endl;
+    cin >> a>>b;
+
+    cout<<"The prime number of a is :"<<prime(a)<<endl;
+    cout<<"The Factorial  of a is :"<<fact(a)<<endl;
+    cout<<"The prime number of b is :"<<prime(b)<<endl;
+    cout<<"The Factorial  of  b is :"<<fact(b)<<endl;
+    cout<<"The prime number of b-a is :"<<prime(b-a)<<endl;
+    cout<<"The Factorial of b-a is :"<<fact(b-a)<<endl;
+    
     return 0 ;
 }
---
+++ Lecture_18_03.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+void Swap( int &x, int &y ) // parameter 
+{
+    int z ;
+    z=x;
+    x=y;
+    y=z;
+}
+void Swap ( float &x , float &y)
+{
+    float z;
+    z=x;
+    x=y;
+    y=z;
+}
+int main()
+{
+    // Swap two numbers
+    int a, b;
+    cout<< " Enter two numbers "<<endl ;
+    cin>> a>> b;
+
+    Swap(a,b);
+    cout<<a<<" "<<b;
+
+    float f1, f2;
+
+    f1 = 3.14, f2 = 2.414;
+    Swap(f1,f2);
+    cout << f1<<f2;
+
+    return 0 ;
+}
